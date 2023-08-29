8b1605334d5879377882b4ab85af5553e7a0f3c5
Nipa <Niilo.urpola@metropolia.fi>
2023-08-29 13:30:12 +0300

    säätöä

--- App.js
+++ App.js
@@ -1,7 +1,46 @@
 import { StatusBar } from "expo-status-bar";
-import { Platform } from "react-native";
-import { SafeAreaView, StyleSheet } from "react-native";
-import List from "./components/List";
+import {
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Image,
+  View,
+} from "react-native";
+
+const mediaArray = [
+  {
+    key: "0",
+    title: "Title 1",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros.",
+    thumbnails: {
+      w160: "http://placekitten.com/160/161",
+    },
+    filename: "http://placekitten.com/2048/1920",
+  },
+  {
+    key: "1",
+    title: "Title 2",
+    description:
+      "Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. ",
+    thumbnails: {
+      w160: "http://placekitten.com/160/164",
+    },
+    filename: "http://placekitten.com/2041/1922",
+  },
+  {
+    key: "2",
+    title: "Title 3",
+    description:
+      "Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. ",
+    thumbnails: {
+      w160: "http://placekitten.com/160/167",
+    },
+    filename: "http://placekitten.com/2039/1920",
+  },
+];
 
 const App = () => {
   return (
@@ -14,10 +53,8 @@ const App = () => {
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: Platform.OS === "android" ? 25 : 0,
-    flex: 5,
-    padding: 20,
-    backgroundColor: "white",
+    flex: 1,
+    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
