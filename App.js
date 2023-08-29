d1ac0752c22509ec9b387dead0429bd151a1826e
Nipa <Niilo.urpola@metropolia.fi>
2023-08-29 13:30:12 +0300

    säätöä

--- App.js
+++ App.js
@@ -1,26 +1,13 @@
-import {StatusBar} from 'expo-status-bar';
-import {Platform} from 'react-native';
-import {SafeAreaView, StyleSheet} from 'react-native';
-import List from './components/List';
+import { StatusBar } from "expo-status-bar";
+import Home from "./views/Home";
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <List />
+    <>
+      <Home></Home>
       <StatusBar style="auto" />
-    </SafeAreaView>
+    </>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    marginTop: Platform.OS === 'android' ? 25 : 0,
-    flex: 5,
-    padding: 20,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default App;
